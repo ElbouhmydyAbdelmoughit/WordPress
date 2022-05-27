4e0a7b14171945721dc177019688c2aefd104bce
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-05-27 15:24:43 +0000

    added plugin RGPD

---
+++ E-commerce-website/wp-content/plugins/ContactUs/ContactUs.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: ContactUs
+ * Description: this plugin contain the description and the information of the developer.
+ * version: 0.1
+ * Author: Elbouhmydy Abdelmoughit
+ */
+
+function HtmlForm()
+{
+}
